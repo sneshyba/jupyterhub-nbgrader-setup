facb8ce8c3dcc6fee9d86199bd9fd773715a6696
Daniel Neshyba-Rowe <rowedanielp@gmail.com>
2026-01-15 23:54:10 -0800

    Make entrypoint give users ownership over their whole directory (recursively)

--- docker_build/entrypoint.sh
+++ docker_build/entrypoint.sh
@@ -8,7 +8,7 @@ while read -r username; do
     # make user home directories if not already made
     if [ ! -d "/home/$username" ]; then
         mkdir "/home/$username"
-        chown "$username:$username" "/home/$username"
+        chown -R "$username:$username" "/home/$username"
         chmod 700 "/home/$username"
     fi
     # add users to nbgrader if not already in database
@@ -21,8 +21,8 @@ done < /srv/jupyterhub/usernames
 if [ ! -f "/home/instructor/.jupyter" ]; then
     mkdir -p /home/instructor/.jupyter
     cp /etc/jupyter/nbgrader_config.py /home/instructor/.jupyter/.
-    chown -R "instructor:instructor" /home/instructor/.jupyter/.
 fi
+chown -R "instructor:instructor" /home/instructor/
 
 # start jupyterhub
 jupyterhub -f /srv/jupyterhub/jupyterhub_config.py
