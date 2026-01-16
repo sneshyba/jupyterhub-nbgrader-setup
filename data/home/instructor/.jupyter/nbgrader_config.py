bbf4d88e92ac0e19f2e4f2e59bc91ba170e25d51
Steven Neshyba <nesh@pugetsound.edu>
2026-01-16 10:48:30 -0800

    Delete data directory

--- data/home/instructor/.jupyter/nbgrader_config.py
+++
@@ -1,6 +0,0 @@
-c = get_config()
-c.CourseDirectory.course_id = "course"
-c.CourseDirectory.root = f"/home/instructor/{c.CourseDirectory.course_id}"
-c.Exchange.root = "/srv/nbgrader/exchange"
-c.NbGrader.logfile = "/home/instructor/logfile.txt"
-c.ClearSolutions.code_stub = {"python": "# Your code here\n"}
