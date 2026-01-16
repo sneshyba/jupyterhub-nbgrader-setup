a3882aa20d3de4b672790d297315c342b80b085c
Daniel Neshyba-Rowe <rowedanielp@gmail.com>
2026-01-16 00:23:35 -0800

    Get nbgrader database access working

--- docker_build/entrypoint.sh
+++ docker_build/entrypoint.sh
@@ -3,6 +3,22 @@
 # get existing nbgrader students
 export students=$(nbgrader db student list 2> /dev/null | tail -n +2 | awk '{print $1}')
 
+# set up instructor
+if [ ! -d "/home/instructor" ]; then
+    mkdir -p /home/instructor/
+fi
+if [ ! -d "/home/instructor/$(cat /srv/jupyterhub/course_name)" ]; then
+    mkdir -p /home/instructor/$(cat /srv/jupyterhub/course_name)
+fi
+if [ ! -d "/home/instructor/.jupyter" ]; then
+    mkdir -p /home/instructor/.jupyter
+    cp /etc/jupyter/nbgrader_config.py /home/instructor/.jupyter/.
+fi
+if [ ! -f "/home/instructor/logfile.txt" ]; then
+    touch /home/instructor/logfile.txt
+fi
+chown -R "instructor:instructor" /home/instructor/
+
 
 while read -r username; do
     # make user home directories if not already made
@@ -16,19 +32,6 @@ while read -r username; do
         nbgrader db student add $username
     fi
 done < /srv/jupyterhub/usernames
-
-# set up instructor
-if [ ! -f "/home/instructor/logfile.txt" ]; then
-    touch /home/instructor/logfile.txt
-fi
-if [ ! -d "/home/instructor/$(cat /srv/jupyterhub/course_name)" ]; then
-    mkdir -p /home/instructor/$(cat /srv/jupyterhub/course_name)
-fi
-if [ ! -d "/home/instructor/.jupyter" ]; then
-    mkdir -p /home/instructor/.jupyter
-    cp /etc/jupyter/nbgrader_config.py /home/instructor/.jupyter/.
-fi
-chown -R "instructor:instructor" /home/instructor/
 
 # start jupyterhub
 jupyterhub -f /srv/jupyterhub/jupyterhub_config.py
--- docker_build/jupyterhub_config/nbgrader_config.py
+++ docker_build/jupyterhub_config/nbgrader_config.py
@@ -1,7 +1,7 @@
 c = get_config()
 
 # read the course name from file to keep things consistent
-c.CourseDirectory.course_id = open("/srv/jupyterhub/course_name").read()
+c.CourseDirectory.course_id = open("/srv/jupyterhub/course_name").read().strip()
 
 # Set the root course directory. Note that this directory is created by entrypoint.sh,
 # so if you change it here it'll have to be changed there too.
